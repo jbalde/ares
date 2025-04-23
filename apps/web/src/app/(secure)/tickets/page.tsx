0cf9a9a7f4df2efd22143910f8ce5c0a3a45baf7
Juan Benavides Romero <jbalde@gmail.com>
2025-04-23 22:43:54 +0200

    tickets page with table

---
+++ apps/web/src/app/(secure)/tickets/page.tsx
@@ -0,0 +1,26 @@
+import { Button, Card, Col, Row, Space } from "antd";
+import TicketsTableComponent from "./tickets-table";
+import Link from "next/link";
+
+export default function Home() {
+	return (
+		<>
+			<Row gutter={[16, 16]}>
+				<Col span={24}>
+					<h1>Tickets</h1>
+				</Col>
+				<Col span={24}>
+					<Link href="/tickets/create">
+						<Button type="primary">Create ticket</Button>
+					</Link>
+				</Col>
+				<Col span={24}>
+					<Card variant="borderless">
+						<h2>Open tickets</h2>
+						<TicketsTableComponent />
+					</Card>
+				</Col>
+			</Row>
+		</>
+	);
+}
