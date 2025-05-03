d0265c098c9880c7b2fd85e19b5a1cd1e21a46db
Juan Benavides Romero <jbalde@gmail.com>
2025-05-03 14:41:50 +0200

    first box on dashboard and cache tickets query

--- apps/web/src/app/(secure)/dashboard/page.tsx
+++ apps/web/src/app/(secure)/dashboard/page.tsx
@@ -1,9 +1,80 @@
 import { Card, Col, Row, Statistic } from "antd";
-import { ArrowUpOutlined } from "@ant-design/icons";
+import {
+	ArrowDownOutlined,
+	ArrowUpOutlined,
+	SwapOutlined,
+	VerticalAlignMiddleOutlined,
+} from "@ant-design/icons";
 import TicketGraphComponent from "./ticket-graph";
 import TicketsTableComponent from "./tickets-table";
+import { PrismaClient } from "@/db/prisma";
+import { unstable_cache } from "next/cache";
 
-export default function Home() {
+const getTicketsFromMonth = async (prevMonths: number) => {
+	const prisma = new PrismaClient();
+
+	const start = new Date(
+		new Date().setDate(new Date().getDate() - 30 * prevMonths)
+	);
+	const end = new Date(
+		new Date().setDate(new Date().getDate() - 30 * (prevMonths + 1))
+	);
+
+	const tickets = await prisma.ticket.findMany({
+		where: {
+			createdAt: {
+				gte: start,
+				lte: end,
+			},
+		},
+	});
+
+	return tickets;
+};
+
+const getTicketsLastsMonths = unstable_cache(
+	async () => {
+		const prevTickets = await getTicketsFromMonth(1);
+		const prev2Tickets = await getTicketsFromMonth(2);
+
+		const tickets = {
+			recents: prevTickets.filter((ticket) => ticket.status === "open"),
+			total: {
+				value: prevTickets.length,
+				percentage:
+					(prevTickets.length - prev2Tickets.length) / prev2Tickets.length,
+			},
+		};
+
+		return tickets;
+	},
+	["last-month-tickets"],
+	{ revalidate: 300, tags: ["last-month-tickets"] }
+);
+
+export default async function DashboardPage() {
+	const { recents, total } = await getTicketsLastsMonths();
+
+	const isUpOrDown = (value: number) => {
+		let icon;
+		let color;
+
+		if (value > 0) {
+			icon = <ArrowUpOutlined />;
+			color = "#3f8600";
+		} else if (value < 0) {
+			icon = <ArrowDownOutlined />;
+			color = "#cf1322";
+		} else {
+			icon = <VerticalAlignMiddleOutlined />;
+		}
+
+		return {
+			icon,
+			color,
+		};
+	};
+
 	return (
 		<>
 			<Row gutter={[16, 16]}>
@@ -13,11 +84,15 @@ export default function Home() {
 				<Col span={6}>
 					<Card variant="borderless">
 						<Statistic
-							title="Total tickets"
-							value={120}
+							title={
+								<>
+									<strong>{total.value}</strong> tickets last month
+								</>
+							}
+							value={Number(total.percentage)}
 							precision={0}
-							valueStyle={{ color: "#3f8600" }}
-							prefix={<ArrowUpOutlined />}
+							valueStyle={{ color: isUpOrDown(total.percentage).color }}
+							prefix={isUpOrDown(total.percentage).icon}
 							suffix="%"
 						/>
 					</Card>
@@ -25,7 +100,7 @@ export default function Home() {
 				<Col span={6}>
 					<Card variant="borderless">
 						<Statistic
-							title="Open tickets"
+							title="Open tickets last month"
 							value={40}
 							precision={0}
 							valueStyle={{ color: "#3f8600" }}
@@ -37,7 +112,7 @@ export default function Home() {
 				<Col span={6}>
 					<Card variant="borderless">
 						<Statistic
-							title="Average response time"
+							title="Average response time last month"
 							value={3.5}
 							precision={1}
 							valueStyle={{ color: "#3f8600" }}
@@ -49,7 +124,7 @@ export default function Home() {
 				<Col span={6}>
 					<Card variant="borderless">
 						<Statistic
-							title="SLA compliance"
+							title="SLA compliance last month"
 							value={90.56}
 							precision={2}
 							valueStyle={{ color: "#3f8600" }}
@@ -67,7 +142,7 @@ export default function Home() {
 				<Col span={24}>
 					<Card variant="borderless">
 						<h2>Recent tickets</h2>
-						<TicketsTableComponent />
+						<TicketsTableComponent tickets={recents} />
 					</Card>
 				</Col>
 			</Row>
